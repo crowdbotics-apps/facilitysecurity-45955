2d4e669b33cbef7d6c7b5d6509bd4b2f7469bfd1
Crowdbotics <team@crowdbotics.com>
2024-02-05 18:11:20 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "facilitysecurity-45955",
+  url: "https://facilitysecurity-45955.botics.co",
+  api: "https://facilitysecurity-45955.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
